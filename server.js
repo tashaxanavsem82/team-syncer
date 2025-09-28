64237675ed9b36f3e23e70f72fb80c01eec805e2
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-28 08:34:25 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Team Syncer API!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
